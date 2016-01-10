a91b7e6a3b201facbe64454184bcacef4f2ae372
抱鹤 <baohe.oyt@alibaba-inc.com>
2016-01-11 00:03:11 +0800

    seajs

---
+++ s_seajs/sea-modules/mod2.js
@@ -0,0 +1,18 @@
+// 所有模块都通过 define 来定义
+define(function(require, exports, module) {
+
+    // 通过 require 引入依赖
+    var $ = require('jquery');
+    var Spinning = require('./spinning');
+
+    // 通过 exports 对外提供接口
+    exports.doSomething = function() {
+        alert('doSomething2');
+    };
+
+    // 或者通过 module.exports 提供整个接口
+    module.exports = function() {
+        alert('export2');
+    }
+
+});
